bd49b9b2e06b9150a7572814cc7ca1c747c4a3f5
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-23 16:27:10 +0530

    feat(jwt-utils): create jwt utils to verify token with tests

---
+++ src/utils/__test__/jwt.util.test.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const { TokenExpiredError } = require('jsonwebtoken');
+const { jwtVerify }  = require('../../utils/jwt.util');
+
+describe('jwtVerify Util',  () => {
+
+  it('should throw error when jwtToken is expired', async () => {
+    const mockJwtToken = "161t2357647325";
+    jest.spyOn(jwt, 'verify').mockImplementation((tooken,secret,callback) => { callback(new TokenExpiredError(),undefined) });
+    try{
+        await jwtVerify(mockJwtToken);
+    } catch(error){
+        expect(error.name).toEqual('TokenExpiredError');
+    }
+  });
+
+  it('should return userdata', async () => {
+    const mockJwtToken = "161t2357647325";
+    const mockUserName = "appy385";
+    jest.spyOn(jwt, 'verify').mockImplementation((tooken,secret,callback) => { callback(undefined, {username: mockUserName}) });
+    const data = await jwtVerify(mockJwtToken);
+    expect(data).toEqual({username: mockUserName});
+   
+  });
+});
