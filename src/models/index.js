c25cea23dcd97784e611f1f4a0876f0f023887c3
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-24 09:33:38 +0530

    feat(database-config): update data abse configurations to use env

--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+const appRoot = require('app-root-path');
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.json`)[env];
+const config = require(`${appRoot}/config/config.js`)[env];
 const db = {};
 
 let sequelize;
