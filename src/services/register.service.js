ec2fc583e475c7e42176450b3c69b4613a74603e
swetha <swetha.gumpena0607@gmail.com>
2021-03-29 10:54:31 +0530

    feat: add password encryption

--- src/services/register.service.js
+++ src/services/register.service.js
@@ -1,10 +1,12 @@
-/* eslint-disable camelcase */
+const bcrypt = require('bcrypt');
 const { User } = require('../models');
 
 const createUser = async (username, password, user_details) => {
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
   const userDetails = await User.create({
     username,
-    password,
+    password: hashedPassword,
     user_details,
   });
   return userDetails;
